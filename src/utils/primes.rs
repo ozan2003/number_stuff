80b888015ac67c89c45e1af3e9854af45873b67a
Ozan Malci <ozan41094@gmail.com>
2025-02-13 17:57:18 +0300

    Add prime and factor utility modules with k determination function

---
+++ src/utils/primes.rs
@@ -0,0 +1,44 @@
+use bit_vec::BitVec;
+use rand::Rng;
+use rug::{Complete, Integer};
+
+/// Determine the number of iterations `k` based on the magnitude of `num`.
+fn determine_k(num: &Integer) -> u32
+{
+    if *num < Integer::i_pow_u(10, 3).complete()
+    {
+        5
+    }
+    else if *num < Integer::i_pow_u(10, 6).complete()
+    {
+        10
+    }
+    else if *num < Integer::i_pow_u(10, 9).complete()
+    {
+        20
+    }
+    else
+    {
+        50
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[test]
+    fn test_determine_k()
+    {
+        assert_eq!(determine_k(&Integer::parse("100").unwrap().complete()), 5); // < 10^3
+        assert_eq!(determine_k(&Integer::parse("999").unwrap().complete()), 5);
+        assert_eq!(determine_k(&Integer::parse("1000").unwrap().complete()), 10); // < 10^6
+        assert_eq!(determine_k(&Integer::parse("999999").unwrap().complete()), 10);
+        assert_eq!(determine_k(&Integer::parse("1000000").unwrap().complete()), 20); // < 10^9
+        assert_eq!(determine_k(&Integer::parse("999999999").unwrap().complete()), 20);
+        assert_eq!(determine_k(&Integer::parse("1000000000").unwrap().complete()), 50); // >= 10^9
+        assert_eq!(determine_k(&Integer::parse("999999999999").unwrap().complete()), 50);
+        assert_eq!(determine_k(&Integer::parse("1000000000000").unwrap().complete()), 50);
+    }
+}
