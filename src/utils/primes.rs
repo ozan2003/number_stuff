776ee79bf20bcb50d3b575f1a9d4cabc8a314af7
Ozan Malci <ozan41094@gmail.com>
2025-02-13 18:20:19 +0300

    formatting

--- src/utils/primes.rs
+++ src/utils/primes.rs
@@ -34,11 +34,45 @@ mod tests
         assert_eq!(determine_k(&Integer::parse("100").unwrap().complete()), 5); // < 10^3
         assert_eq!(determine_k(&Integer::parse("999").unwrap().complete()), 5);
         assert_eq!(determine_k(&Integer::parse("1000").unwrap().complete()), 10); // < 10^6
-        assert_eq!(determine_k(&Integer::parse("999999").unwrap().complete()), 10);
-        assert_eq!(determine_k(&Integer::parse("1000000").unwrap().complete()), 20); // < 10^9
-        assert_eq!(determine_k(&Integer::parse("999999999").unwrap().complete()), 20);
-        assert_eq!(determine_k(&Integer::parse("1000000000").unwrap().complete()), 50); // >= 10^9
-        assert_eq!(determine_k(&Integer::parse("999999999999").unwrap().complete()), 50);
-        assert_eq!(determine_k(&Integer::parse("1000000000000").unwrap().complete()), 50);
+        assert_eq!(
+            determine_k(&Integer::parse("999999").unwrap().complete()),
+            10
+        );
+        assert_eq!(
+            determine_k(&Integer::parse("1000000").unwrap().complete()),
+            20
+        ); // < 10^9
+        assert_eq!(
+            determine_k(
+                &Integer::parse("999999999")
+                    .unwrap()
+                    .complete()
+            ),
+            20
+        );
+        assert_eq!(
+            determine_k(
+                &Integer::parse("1000000000")
+                    .unwrap()
+                    .complete()
+            ),
+            50
+        ); // >= 10^9
+        assert_eq!(
+            determine_k(
+                &Integer::parse("999999999999")
+                    .unwrap()
+                    .complete()
+            ),
+            50
+        );
+        assert_eq!(
+            determine_k(
+                &Integer::parse("1000000000000")
+                    .unwrap()
+                    .complete()
+            ),
+            50
+        );
     }
 }
