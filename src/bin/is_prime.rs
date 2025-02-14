6793ce2bbd8fbef8df0e1cfc192c21d0c0cf794d
Ozan Malci <ozan41094@gmail.com>
2025-02-14 19:20:10 +0300

    Add is_prime binary to check primality of a number with timing

---
+++ src/bin/is_prime.rs
@@ -0,0 +1,34 @@
+use number_stuff::utils::primes::is_prime;
+use rug::Integer;
+use std::env::args;
+use std::process::exit;
+use std::time::Instant;
+
+fn main()
+{
+    let args: Vec<String> = args().collect();
+    if args.len() < 2
+    {
+        eprintln!("Usage: {} <number>", args[0]);
+        exit(1);
+    }
+
+    let num: Integer = args[1]
+        .parse()
+        .expect("Couldn't parse number.");
+
+    let t0 = Instant::now();
+    let is_prime = is_prime(&num);
+    let elapsed = t0.elapsed();
+
+    if is_prime
+    {
+        println!("{} is prime", num);
+    }
+    else
+    {
+        println!("{} is not prime", num);
+    }
+
+    println!("Took: {:?}", elapsed);
+}
