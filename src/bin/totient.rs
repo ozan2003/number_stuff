bf4d43430dc22ae2d700dd49238de1b8915ad12d
Ozan Malci <ozan41094@gmail.com>
2025-02-14 19:20:31 +0300

    Add totient binary to calculate and time the Euler's totient function

---
+++ src/bin/totient.rs
@@ -0,0 +1,25 @@
+use number_stuff::utils::factors::totient;
+use std::env::args;
+use std::process::exit;
+use std::time::Instant;
+
+fn main()
+{
+    let args: Vec<String> = args().collect();
+    if args.len() < 2
+    {
+        eprintln!("Usage: {} <number>", args[0]);
+        exit(1);
+    }
+
+    let num: i64 = args[1]
+        .parse()
+        .expect("Couldn't parse number.");
+
+    let t0 = Instant::now();
+    let totient = totient(num);
+    let elapsed = t0.elapsed();
+
+    println!("f({}) = {}", num, totient);
+    println!("Took: {:?}", elapsed);
+}
