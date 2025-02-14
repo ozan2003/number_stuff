f05c71266507ce2958bd0e1881c62cdbad7a500b
Ozan Malci <ozan41094@gmail.com>
2025-02-14 19:20:23 +0300

    Add sieve binary to generate and print prime numbers up to a given limit

---
+++ src/bin/sieve.rs
@@ -0,0 +1,32 @@
+use number_stuff::utils::primes::Primes;
+use std::env::args;
+use std::process::exit;
+
+fn main()
+{
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 2
+    {
+        eprintln!("Usage: {} <number>", args[0]);
+        exit(1);
+    }
+
+    let n = match args[1].parse()
+    {
+        | Ok(n) => n,
+        | Err(_) =>
+        {
+            eprintln!("Invalid input: {}", args[1]);
+            exit(1);
+        }
+    };
+
+    let primes = Primes::new(n);
+
+    println!("Primes up to {}:", n);
+    for p in primes.iter()
+    {
+        println!("{}", p);
+    }
+}
